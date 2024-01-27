8254a2f513ed42bb4611704bf13db9c89a9e794c
Genie Park <genie0320@gmail.com>
2024-01-28 00:04:31 +0900

    conversational_rag_test

--- constants.py
+++
@@ -1,8 +0,0 @@
-import os
-from chromadb.config import Settings
-
-CHROMA_SETTINGS = Settings(
-    # chroma_db_impl="duckdb+parquet",  This option is deprecated. Chroma 0.4~
-    persist_directory="db",
-    anonymized_telemetry=False,
-)
